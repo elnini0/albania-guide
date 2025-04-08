9abd13dc51f8580f74c57e3bff8f30930c88d1e5
Asaf Ninio <asafninio@gmail.com>
2025-04-08 14:40:23 +0300

    fix: remove static export config causing 404s

--- next.config.js
+++ next.config.js
@@ -3,9 +3,7 @@ const nextConfig = {
   reactStrictMode: true,
   images: {
     domains: ['localhost', 'images.unsplash.com'],
-    unoptimized: true,
-  },
-  output: 'export',
+  }
 }
 
 module.exports = nextConfig 
