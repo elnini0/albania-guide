6dbbc8fdb0d3750e2ecb429fd4a10f173545814e
Asaf Ninio <asafninio@gmail.com>
2025-04-08 14:38:49 +0300

    fix: remove unused imports causing ESLint errors

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,3 @@
-import Image from 'next/image';
-import Link from 'next/link';
-import { Footer, AttractionCard, DetourCard, DaySidebar } from './components/common';
-
 export default function Home() {
   return (
     <div>
